db3a5ba6ba01f1bcd5d7c123c4451a079be07d26
Saed Silva Sousa <saed.sousa@hotmail.com>
2024-05-04 11:29:14 -0300

    Service layer, component registration

---
+++ src/main/java/com/saed/dev/services/UserService.java
@@ -0,0 +1,24 @@
+package com.saed.dev.services;
+
+import com.saed.dev.entities.User;
+import com.saed.dev.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+    public User findById (Long id) {
+       Optional<User> user = repository.findById(id); // Retorna um objeto do tipo Optional que pode ou não conter um objeto do tipo User
+       return user.get();
+    }
+}
